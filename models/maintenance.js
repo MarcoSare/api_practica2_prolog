9c5d8fd5d4a63013da9c39c51d68ab3db8023997
David Portador <62720642+DavidPortador@users.noreply.github.com>
2023-03-24 03:18:30 -0600

    Tipo de mantenimiento

--- models/maintenance.js
+++ models/maintenance.js
@@ -5,6 +5,7 @@ const maintenance_schema = mongoose.Schema({
     id_computer: {type: mongoose.Types.ObjectId, ref:"computer"},
     date: {type: String, require: true},
     support: {type: mongoose.Types.ObjectId, ref:"user"},
+    type: {type: String, require: true},
 }, {'collection':'maintenance'})
 
 const maintenance = mongoose.model('maintenance', maintenance_schema);
--- routes/maintenance_routes.js
+++ routes/maintenance_routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { ObjectId } = require("mongodb");
 const router = express.Router();
 const maintenance = require("../models/maintenance");
+const historic = require("../models/historic");
+
 
 router.get("/maintenance", async (req, res) => {
   try {
@@ -18,7 +20,8 @@ router.post("/maintenance", async (req, res) => {
         id_area: req.body.id_area,
         id_computer: req.body.id_computer,
         date: req.body.date,
-        support: req.body.support
+        support: req.body.support,
+        type: req.body.type
       });
       const saved = await newMaintenance.save();
       if (saved) 
@@ -38,7 +41,8 @@ router.put("/maintenance/:id", async (req, res) => {
         id_area: req.body.id_area,
         id_computer: req.body.id_computer,
         date: req.body.date,
-        support: req.body.support
+        support: req.body.support,
+        type: req.body.type
       }
     );
     return res.send(JSON.parse('{"message" : "Successful"}'));
@@ -49,10 +53,28 @@ router.put("/maintenance/:id", async (req, res) => {
 
 router.delete("/maintenance/:id", async (req, res) => {
   try {
+
+    const maint = await maintenance.findOne({ _id: req.params.id });
+
+    const saved = await maint.save();
+      if (saved) 
+        return res.send(JSON.parse('{"message" : "Successful"}'));
+      else
+        return res.status(400).send(JSON.parse('{"message" : "No successful"}'));
+
     const deleteMaintenance = await maintenance.deleteOne({ _id: req.params.id });
     res.send(JSON.parse('{"message" : "Successful"}'));
   } catch (error) {
     res.send(error);
+  }
+});
+
+router.get("/historic", async (req, res) => {
+  try {
+    const historic = await historic.find({});
+    res.send(historic);
+  } catch (error) {
+    return res.send(error.message);
   }
 });
 
